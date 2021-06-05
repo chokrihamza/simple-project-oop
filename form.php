a1feb0a4012bdef4a1df653cd43ed394eadd2213
chokrihamza <chokrihamza1994@gmail.com>
2021-06-05 10:22:49 +0200

    adding bootstrap heritage class

--- form.php
+++ form.php
@@ -2,21 +2,29 @@
 class Form{
   private $data;
   public $surround='p';
-
+  static $color = '';
+  
   public function __construct($data=array())
   {
     $this->data=$data;
   }
-  private function surround($html){
-   return "<{$this->surround}>{$html}</{$this->surround}>";
+  protected function surround($html)
+  {
+    return "<{$this->surround} >{$html}</{$this->surround}>";
   }
-  private function get_value($index){
+  protected function get_value($index)
+  {
    return isset($this->data[$index]) ?$this->data[$index]:null;
   }
   public function input($name){
     return $this->surround('<input type="text" name="'.$name.'" value="'.$this->get_value($name).'"/>')  ;
   }
+  protected function col()
+  {
+    return self::$color;
+  }
+
   public function submit(){
-    return $this->surround("<button type='submit'>Send</button>");
+    return $this->surround("<button style='background-color:{$this->col()}' type='submit'>Send</button>");
   }
 }
