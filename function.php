16961c9869b5cd45f6ea97a2ee06edb9e0165b52
chokrihamza <chokrihamza1994@gmail.com>
2021-06-06 19:01:25 +0200

    work with closure1

--- function.php
+++ function.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $a = 10;
 $add = function ($a, $b) {
   return $a + $b;
@@ -12,3 +14,23 @@ $adding = function () use ($add) {
 
 $adding();
 echo $a;
+
+///
+class Student
+{
+  private static $id = 30;
+  private $name = "hamza";
+}
+
+$get_id = static function () {
+  return Student::$id;
+};
+
+$get_name = function () {
+  return $this->name;
+};
+
+$student = new Student;
+
+echo (Closure::bind($get_id, null, 'Student'))();
+echo (Closure::bind($get_name, new Student, 'Student'))();
